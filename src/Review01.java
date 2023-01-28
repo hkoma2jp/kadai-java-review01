5f71a09fc6baeddd8803bd15e4f32412a5a9fadf
hkoma2jp <h.koma2.jp@gmail.com>
2023-01-28 14:17:19 +0900

    Lesson3　課題　税込価格を計算しよう

---
+++ src/Review01.java
@@ -0,0 +1,21 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        // 商品価格を入力
+        int price = 777;
+        // 税込価格の算出
+        int amount = price + (int)tax(price);
+        // 出力
+        System.out.println(price + "円の商品の税込価格は" + amount + "円（消費税は" + (int)tax(price) + "円）です。");
+    }
+
+    public static double tax(int price) {
+        // 税率を設定
+        double rate = 0.1;
+        // 消費税額（小数点以下切り捨て）
+        double tax = Math.floor(price * rate);
+        return tax;
+    }
+
+}
